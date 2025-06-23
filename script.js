83efdf436cf2076eaa981ddeabdee6d9966ce972
Vaishnavi  Nikam <150416104+VaishDataGeek@users.noreply.github.com>
2025-06-23 12:58:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	c = Number(c); // Ensure input is a number
+
+  const q = Math.floor(c / 25);
+  c %= 25;
+
+  const d = Math.floor(c / 10);
+  c %= 10;
+
+  const n = Math.floor(c / 5);
+  c %= 5;
+
+  const p = c; 
+
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
